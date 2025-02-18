fb018ade02771daaaab280b0aa0f3c0179daf573
Fire Coin <maxim.m.kollar@gmail.com>
2025-02-18 16:02:08 +0100

    message

---
+++ formater.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+using std::cout, std::cin, std::getline, std::string,
+std::ofstream, std::ifstream;
