b6b7c21e97329d298cd85026518ee9b2ebd3630e
Fire Coin <maxim.m.kollar@gmail.com>
2025-02-18 16:34:24 +0100

    Formater now replaces content of input file with formated code, it is irreversible. Also now it ignores any leading white spaces.

--- f2.cpp
+++ f2.cpp
@@ -9,8 +9,8 @@ using namespace std;
 namespace fs = std::filesystem;
 const string HFILE = "helpingFile.hsf";
 
-//vector<string> split(string line, char delimiter= ' ');
 int format(string);
+string strip(string);
 
 int main() {
 	format("data1.txt");
@@ -18,26 +18,31 @@ int main() {
 }
 
 int format(string inputFile) {
-	ifstream fin(inputFile);
-	if (fs::exists(fs::path(HFILE))) {
-		remove(fs::path(HFILE));
+	ifstream fin(inputFile); // File from which program will read
+	// filesystem paths
+	fs::path helpFile(HFILE); // Path to helping file
+	fs::path readFile(inputFile); // Path to input file
+
+	if (fs::exists(helpFile)) { // Removes file if it already exists
+		remove(helpFile);
 	}
-	ofstream fon(HFILE);
-	string line;
-	int intent = 0;
-	bool back = false;
-	bool inStr = false;
-	char openedWith = NULL;
-	int curLine = 0;
-	int changed = 0;
+	ofstream fon(HFILE); // Opening helping file
+	string line; // Buffer to read from inputFile
+	int intent = 0; // Intent on current line
+	int lastIntent = 0; // Intent on last line
+	bool back = false; // Indicates if escape caracter with backslash is present
+	bool inStr = false; // True if currently inside of a string
+	char openedWith = NULL; // Stores whether string was opened with ' or "
+	int curLine = 0; // Current line
+	int changed = 0; // Last index in vector intents
 	vector<pair<int, int>> intents = {pair(0, 0)};
+	bool significant = false; // Is true 
+
 	if (fin.is_open() && fon.is_open()) {
 		while (getline(fin, line)) {
 			curLine++;
-			string lineNow(intent * 4, ' ');
-			lineNow += line;
-			fon << lineNow << '\n';
 			for (char i : line) {
+				line = strip(line);
 				if (i == '\\') {
 					back = !back;
 					continue;
@@ -66,21 +71,27 @@ int format(string inputFile) {
 					continue;
 				if (i == '{') {
 					intent++;
-					//intents.push_back(pair(curLine, intent));
 					continue;
 				}
 				if (i == '}' && intent > 0) {
 					intent--;
-					//intents.push_back(pair(curLine, intent));
 					continue;
 				}
 			}
+			// Generates intents
+			string lineNow(min(intent, lastIntent), '\t');
+			lineNow += line;
+			fon << lineNow << '\n';
+			lastIntent = intent;
+			
+			// Used for debuging
 			if (intent != intents[changed].second) {
 				intents.push_back(pair(curLine, intent));
 				changed++;
 			}
 			cout << intent << intents[changed].second << '\n';
 		}
+		// Used for debuging
 		for (auto i : intents) {
 			cout << i.first << ' ' << i.second << '\n';
 		}
@@ -89,6 +100,18 @@ int format(string inputFile) {
 	}
 	fin.close();
 	fon.close();
+	
+	fs::remove(readFile);
+	fs::copy_file(helpFile, readFile);
+	fs::remove(helpFile);
 
 	return 0;
+}
+
+string strip(string str) {
+	int index = 0;
+	while (str[index] == ' ') {
+		index++;
+	}
+	return str.substr(index, str.length() - index);
 }
